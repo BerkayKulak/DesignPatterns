e31fab26f0fe3c455464d1ad492835f4cbb81b5e
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-01-27 10:18:18 +0300

    3. Adım (ProductRepositoryFromSQLServer)

---
+++ WebApp.Strategy/Repositories/ProductRepositoryFromSqlServer.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BaseProject.Models;
+using Microsoft.EntityFrameworkCore;
+using WebApp.Strategy.Models;
+
+namespace WebApp.Strategy.Repositories
+{
+    public class ProductRepositoryFromSqlServer:IProductRepository
+    {
+        private readonly AppIdentityDbContext _context;
+
+        public ProductRepositoryFromSqlServer(AppIdentityDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Product> GetById(string id)
+        {
+            return await _context.Products.FindAsync(id);
+        }
+
+        public async Task<List<Product>> GetAllByUserId(string userId)
+        {
+            return await _context.Products.Where(x => x.UserId == userId).ToListAsync();
+        }
+
+        public async Task<Product> Save(Product product)
+        {
+            await _context.Products.AddAsync(product);
+
+            await _context.SaveChangesAsync();
+
+            return product;
+        }
+
+        public async Task Update(Product product)
+        {
+            _context.Products.Update(product);
+
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Delete(Product product)
+        {
+            // aynı mantık
+            // _context.Entry(product).State = EntityState.Deleted;
+
+            _context.Products.Remove(product);
+
+            await _context.SaveChangesAsync();
+        }
+    }
+}
