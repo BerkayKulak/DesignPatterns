578dc5db9d4eb0c52edf686a81f83ed27dc7bd42
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-01-27 23:22:40 +0300

    UserCardTemplate

--- WebApp.Template/Models/AppUser.cs
+++ WebApp.Template/Models/AppUser.cs
@@ -4,6 +4,8 @@ namespace BaseProject.Models
 {
     public class AppUser:IdentityUser
     {
+        public string PictureUrl { get; set; }
+        public string Description { get; set; }
 
     }
 }
---
+++ WebApp.Template/UserCards/UserCardTemplate.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+using BaseProject.Models;
+
+namespace WebApp.Template.UserCards
+{
+    public abstract class UserCardTemplate
+    {
+        protected AppUser AppUser { get; set; }
+
+        public void SetUser(AppUser appUser)
+        {
+            AppUser = appUser;
+        }
+
+        public string Build()
+        {
+            if (AppUser == null) throw new ArgumentNullException(nameof(AppUser));
+
+            var stringBuilder = new StringBuilder();
+
+            stringBuilder.Append("<div class='card'>");
+
+            stringBuilder.Append(SetPicture());
+
+            stringBuilder.Append($@"<div class='card-body'> 
+            
+                <h5>{AppUser.UserName}</h5>
+                <p>{AppUser.Description}</p>");
+
+            stringBuilder.Append(SetFooter());
+
+            stringBuilder.Append("</div>");
+
+            stringBuilder.Append("</div>");
+
+            return stringBuilder.ToString();
+
+        }
+
+        protected abstract string SetFooter();
+
+        protected abstract string SetPicture();
+
+    }
+}
