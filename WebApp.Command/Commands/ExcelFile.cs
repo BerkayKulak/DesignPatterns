f82e273d3785730bc6018caec163b5c37a7bb9c4
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-01-28 19:03:15 +0300

    2. Adım (ExcelFile Class)

---
+++ WebApp.Command/Commands/ExcelFile.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using ClosedXML.Excel;
+using DocumentFormat.OpenXml.Drawing.Charts;
+
+namespace WebApp.Command.Commands
+{
+    public class ExcelFile<T>
+    {
+        public readonly List<T> _list;
+
+        public string FileName => $"{typeof(T).Name}.xlsx";
+
+        public string FileType => "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+
+        public ExcelFile(List<T> list)
+        {
+            _list = list;
+        }
+
+        public MemoryStream Create()
+        {
+            var wb = new XLWorkbook();
+
+            var ds = new DataSet();
+
+            ds.Tables.Add(GetTable());
+
+            wb.Worksheets.Add(ds);
+
+            var excelMemory = new MemoryStream();
+
+            wb.SaveAs(excelMemory);
+
+            return excelMemory;
+        }
+
+        private System.Data.DataTable GetTable()
+        {
+            var table = new System.Data.DataTable();
+
+            var type = typeof(T);
+
+            type.GetProperties().ToList().ForEach(x =>  table.Columns.Add(x.Name, x.PropertyType));
+
+            _list.ForEach(x =>
+            {
+                var values = type.GetProperties().Select(propertyInfo => propertyInfo.GetValue(x, null)).ToArray();
+
+                table.Rows.Add(values);
+
+            });
+
+            return table;
+
+        }
+    }
+}
