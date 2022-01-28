ce1b5ed0f3dd9216e14156ad45623863fca8fa76
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-01-28 20:44:14 +0300

    5. Adım FileCreateInvoker

--- WebApp.Command/Commands/CreateExcelTableActionCommand.cs
+++ WebApp.Command/Commands/CreateExcelTableActionCommand.cs
@@ -1,6 +1,22 @@
-﻿namespace WebApp.Command.Commands
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Command.Commands
 {
-    public class CreateExcelTableActionCommand
+    public class CreateExcelTableActionCommand<T> : ITableActionCommand
     {
+        private readonly ExcelFile<T> _excelFile;
+
+        public CreateExcelTableActionCommand(ExcelFile<T> excelFile)
+        {
+            _excelFile = excelFile;
+        }
+
+        public IActionResult Execute()
+        {
+            var excelMemoryStream = _excelFile.Create();
+
+            return new FileContentResult(excelMemoryStream.ToArray(),_excelFile.FileType) {FileDownloadName = _excelFile.FileName};
+        }
     }
 }
---
+++ WebApp.Command/Commands/FileCreateInvoker.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using DocumentFormat.OpenXml.Office2010.ExcelAc;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Command.Commands
+{
+    public class FileCreateInvoker
+    {
+        private  ITableActionCommand _tableActionCommand;
+
+        private List<ITableActionCommand> tableActionCommands = new List<ITableActionCommand>();
+
+
+        public void SetCommand(ITableActionCommand tableActionCommand)
+        {
+            _tableActionCommand = tableActionCommand;
+        }
+
+        public void AddCommand(ITableActionCommand tableActionCommand)
+        {
+            tableActionCommands.Add(tableActionCommand);
+        }
+
+        public IActionResult CreateFile()
+        {
+            return _tableActionCommand.Execute();
+        }
+
+        public List<IActionResult> CreateFiles()
+        {
+            return tableActionCommands.Select(x => x.Execute()).ToList();
+        }
+    }
+}
