3a925aaab7ff8e43c337fafa1968f542fddf49f2
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-01-28 21:17:07 +0300

    6. Adım (ProductController)

---
+++ WebApp.Command/Controllers/ProductsController.cs
@@ -0,0 +1,63 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using BaseProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApp.Command.Commands;
+using WebApp.Command.Models;
+
+namespace WebApp.Command.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly AppIdentityDbContext _context;
+
+        public ProductsController(AppIdentityDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Products.ToListAsync());
+        }
+
+        public async Task<IActionResult> CreateFile(int type)
+        {
+            var products = await _context.Products.ToListAsync();
+
+            FileCreateInvoker fileCreateInvoker = new();
+
+
+            EFileType fileType = (EFileType) type;
+
+            switch (fileType)
+            {
+                case EFileType.Excel:
+
+                    ExcelFile<Product> excelFile = new(products);
+
+                    fileCreateInvoker.SetCommand(new CreateExcelTableActionCommand<Product>(excelFile));
+
+                    break;
+
+                case EFileType.Pdf:
+
+                    PdfFile<Product> pdfFie = new(products, HttpContext);
+
+
+                    fileCreateInvoker.SetCommand(new CreatePdfTableActionCommand<Product>(pdfFie));
+
+                    break;
+
+                default:
+
+                    break;
+            }
+
+            return fileCreateInvoker.CreateFile();
+
+
+        }
+    }
+}
