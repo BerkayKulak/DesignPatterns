89e44be3b005f182f660f4f7dd030b3964df5b01
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-01-29 13:16:05 +0300

    discount migrations are added

--- WebApp.Observer/Models/AppIdentityDbContext.cs
+++ WebApp.Observer/Models/AppIdentityDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using WebApp.Observer.Models;
 
 namespace BaseProject.Models
 {
@@ -9,5 +10,7 @@ namespace BaseProject.Models
         {
             
         }
+
+        public DbSet<Discount> Discounts { get; set; }
     }
 }
