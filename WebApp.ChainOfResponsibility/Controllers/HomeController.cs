6fce1b4a580204de5e6ad324af55e398d46452fa
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-01-30 18:52:12 +0300

    chain of responsibility

--- WebApp.ChainOfResponsibility/Controllers/HomeController.cs
+++ WebApp.ChainOfResponsibility/Controllers/HomeController.cs
@@ -6,21 +6,44 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApp.ChainOfResponsibility.ChainOfResponsibility;
+using WebApp.ChainOfResponsibility.Models;
 
 namespace BaseProject.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly AppIdentityDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, AppIdentityDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
             return View();
+        }
+
+        public async Task<IActionResult> SendEmail()
+        {
+            var products = await _context.Products.ToListAsync();
+
+            var excelProcessHandler = new ExcelProcessHandler<Product>();
+
+            var zipFileProcessHandler = new ZipFileProcessHandler<Product>();
+
+            var sendEmailProcessHandler = new SendEmailProcessHandler("product.zip","beko_468@hotmail.com");
+
+            excelProcessHandler.SetNext(zipFileProcessHandler).SetNext(sendEmailProcessHandler);
+
+            excelProcessHandler.Handle(products);
+
+            return View(nameof(Index));
+
         }
 
         public IActionResult Privacy()
