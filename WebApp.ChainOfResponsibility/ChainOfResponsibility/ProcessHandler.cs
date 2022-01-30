fd822a7961b62ea9142a1a32ff9ec32d4f2cd966
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-01-30 14:09:05 +0300

    ProcessHandler

---
+++ WebApp.ChainOfResponsibility/ChainOfResponsibility/ProcessHandler.cs
@@ -0,0 +1,23 @@
+﻿namespace WebApp.ChainOfResponsibility.ChainOfResponsibility
+{
+    public abstract class ProcessHandler:IProcessHandler
+    {
+        private IProcessHandler _nextProcessHandler;
+        public IProcessHandler SetNext(IProcessHandler processHandler)
+        {
+            _nextProcessHandler = processHandler;
+
+            return _nextProcessHandler;
+        }
+
+        public virtual object Handle(object o)
+        {
+            if (_nextProcessHandler != null)
+            {
+                return _nextProcessHandler.Handle(o);
+            }
+
+            return null;
+        }
+    }
+}
