b03def068f163f0b990a971395ce1d4d5f24543e
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-02-02 22:51:49 +0300

    repository ve interfaces classes are added

--- WebApp.Decorator/Startup.cs
+++ WebApp.Decorator/Startup.cs
@@ -11,6 +11,7 @@ using System.Threading.Tasks;
 using BaseProject.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using WebApp.Decorator.Repositories;
 
 namespace BaseProject
 {
@@ -26,6 +27,7 @@ namespace BaseProject
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<IProductRepository, ProductRepository>();
             services.AddDbContext<AppIdentityDbContext>(options =>
             {
                 options.UseSqlServer(Configuration.GetConnectionString("SqlServer"));
