ed607e1cd100ea9151eb561d2a961f04ba8ae85f
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-02-03 10:28:47 +0300

    lazziya resize library added

---
+++ WebApp.Adapter/Services/IAdvanceImageProcess.cs
@@ -0,0 +1,11 @@
+﻿using System.Drawing;
+using System.IO;
+
+namespace WebApp.Adapter.Services
+{
+    public interface IAdvanceImageProcess
+    {// void AddWatermark(string text, string filename, Stream imageStream);
+
+        void AddWatermarkImage(Stream stream, string text, string filePath, Color color, Color outlineColor);
+    }
+}
