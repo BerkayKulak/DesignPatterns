786950e2c29eefa066909fc23905382bcd9d4b99
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-02-03 10:21:17 +0300

    3. adım resime filigran eklemek

--- WebApp.Adapter/Controllers/HomeController.cs
+++ WebApp.Adapter/Controllers/HomeController.cs
@@ -4,18 +4,22 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using WebApp.Adapter.Services;
 
 namespace BaseProject.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly IImageProcess _imageProcess;
+        public HomeController(ILogger<HomeController> logger, IImageProcess imageProcess)
         {
             _logger = logger;
+            _imageProcess = imageProcess;
         }
 
         public IActionResult Index()
@@ -25,6 +29,26 @@ namespace BaseProject.Controllers
 
         public IActionResult Privacy()
         {
+            return View();
+        }
+
+        public IActionResult AddWaterMark()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddWaterMark(IFormFile image)
+        {
+            if (image is {Length: > 0})
+            {
+                var imageMemoryStream = new MemoryStream();
+
+                await image.CopyToAsync(imageMemoryStream);
+
+                _imageProcess.AddWaterMark("Asp.Net Core MVC",image.FileName,imageMemoryStream);
+            }
+
             return View();
         }
 
---
+++ WebApp.Adapter/Services/IImageProcess.cs
@@ -0,0 +1,10 @@
+﻿using System.IO;
+
+namespace WebApp.Adapter.Services
+{
+    public interface IImageProcess
+    {
+        void AddWaterMark(string text, string fileName, Stream imageStream);
+
+    }
+}
